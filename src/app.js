bc5cac418c953ed00288bca43a20fea2b4cc918b
mosluce <mosluce@gmail.com>
2016-04-12 18:15:58 +0800

    加入404及Error Handler

--- src/app.js
+++ src/app.js
@@ -9,9 +9,25 @@ let app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use('/', router);
+
+app.use((req, res, next) => {
+    let error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res) => {
+    let env = app.get('env');
+
+    res.status(error.status || 500)
+        .json({
+            message: error.message || 'Internal Error',
+            error: (env === 'development') ? error : null
+        });
+});
 
 export default app;
